1281ef75e6926152ca6f7e4c0956f248fc30e8e7
Sergey <64882377+WebWat@users.noreply.github.com>
2022-08-16 12:36:35 +0300

    need fix

--- Tree/Controllers/HomeController.cs
+++ Tree/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace Tree.Controllers
         {
             var sessionId = GetSessionId();
 
-            if (!Directory.Exists(Path.Combine(_path, sessionId)))
+            if (!HttpContext.Session.TryGetValue(sessionId, out _))
             {
                 return View(new IndexModel { BeenBefore = false });
             }
@@ -33,67 +33,46 @@ namespace Tree.Controllers
             return View(new IndexModel { BeenBefore = true, SessinId = sessionId });
         }
 
+        [ResponseCache(Location = ResponseCacheLocation.Client, Duration = 60 * 10)]
         public IActionResult Tree(string path)
         {
-            if (!Directory.Exists(Path.Combine(_path, GetSessionId())))
+            if (!HttpContext.Session.TryGetValue(GetSessionId(), out _))
             {
                 return RedirectToAction(nameof(Index));
             }
 
-            var value = HttpContext.Session.GetString(path);
+            string? value = HttpContext.Session.GetString(path);
 
-            if (string.IsNullOrEmpty(value))
-            {
-                _logger.LogInformation($"Adding data to cookies: {path}");
-
-                var list = new List<Models.File>();
-
-                var newPath = Path.Combine(_path, path.Replace('/', '\\'));
-
-                var di = new DirectoryInfo(newPath);
-
-                foreach (var item in di.GetDirectories())
-                {
-                    list.Add(new Models.File { Name = item.Name, IsFolder = true });
-                }
-
-                foreach (var item in di.GetFiles())
-                {
-                    list.Add(new Models.File { Name = item.Name, Length = GetSize(item.Length), IsFolder = false });
-                }
-
-                var vm = new FilesInfo { CurrentPath = path, Files = list };
-
-                HttpContext.Session.SetString(path, JsonSerializer.Serialize(vm));
-
-                return View(vm);
-            }
-
-            _logger.LogInformation($"The data is derived from a cookie: {path}");
+            ArgumentNullException.ThrowIfNull(value);
 
             return View(JsonSerializer.Deserialize<FilesInfo>(value));
         }
 
 
         [HttpPost]
-        [RequestSizeLimit(104_857_600)]
+        [RequestSizeLimit(100 * 1024 * 1024)]
         public async Task<IActionResult> Upload(IndexModel im)
         {
             if (im.File != null)
             {
                 var _userPath = Path.Combine(_path, GetSessionId());
+                var temp = GetSessionId();
 
-                if (Directory.Exists(_userPath))
+                if (HttpContext.Session.TryGetValue(temp, out _))
                 {
-                    var temp = GetSessionId();
                     HttpContext.Session.Clear();
-                    HttpContext.Session.SetString("Id", temp);
 
-                    ClearFolder(_userPath);
-                    Directory.Delete(_userPath);
+                    temp = Guid.NewGuid().ToString();
+
+                    HttpContext.Session.SetString("Id", temp);
                 }
 
                 Directory.CreateDirectory(_userPath);
+
+                _logger.LogInformation("Create directory: " + _userPath);
+                _logger.LogInformation("Start uploading and extracting...");
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
 
                 string zipPath = Path.Combine(_userPath, im.File.FileName);
 
@@ -105,12 +84,25 @@ namespace Tree.Controllers
                 ZipFile.ExtractToDirectory(zipPath, _userPath, entryNameEncoding:
                     Encoding.GetEncoding(System.Globalization.CultureInfo.CurrentCulture.TextInfo.OEMCodePage));
 
-                System.IO.File.Delete(zipPath);
+                stopwatch.Stop();
 
-                return RedirectToAction(nameof(Tree), new { path = GetSessionId() });
+                _logger.LogInformation($"Uploaded and extracted! Total time: {stopwatch.ElapsedMilliseconds / 1000d:f3} s.");
+
+                _logger.LogInformation("Save data in the session delete all downloaded data..");
+
+                stopwatch.Restart();
+
+                System.IO.File.Delete(zipPath);
+                AddToSessionStorage(_userPath);
+                ClearFolder(_userPath);
+                Directory.Delete(_userPath);
+
+                _logger.LogInformation($"Loaded and cleared! Total time: {stopwatch.ElapsedMilliseconds / 1000d:f3} s.");
+
+                return RedirectToAction("tree", new { path = temp });
             }
 
-            return View();
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -152,11 +144,6 @@ namespace Tree.Controllers
         {
             var dir = new DirectoryInfo(path);
 
-            foreach (var fi in dir.GetFiles())
-            {
-                fi.Delete();
-            }
-
             foreach (var di in dir.GetDirectories())
             {
                 ClearFolder(di.FullName);
@@ -164,20 +151,46 @@ namespace Tree.Controllers
             }
         }
 
-        private void AddFolderToCookie(string path)
+        private void AddToSessionStorage(string path)
         {
-            DirectoryInfo dir = new DirectoryInfo(path);
+            FilesInfo item;
 
-            foreach (var fi in dir.GetFiles())
+            List<Models.File> list;
+
+            string newPath;
+
+            DirectoryInfo dir;
+
+            void SetFolder(string path)
             {
-                fi.Delete();
+                list = new List<Models.File>();
+
+                newPath = Path.Combine(_path, path.Replace('/', '\\'));
+
+                dir = new DirectoryInfo(newPath);
+
+                foreach (var item in dir.GetDirectories())
+                {
+                    list.Add(new Models.File { Name = item.Name, IsFolder = true });
+                }
+
+                foreach (var item in dir.GetFiles())
+                {
+                    list.Add(new Models.File { Name = item.Name, Length = GetSize(item.Length), IsFolder = false });
+                    item.Delete();
+                }
+
+                item = new FilesInfo { CurrentPath = path, Files = list };
+
+                HttpContext.Session.SetString(path, JsonSerializer.Serialize(item));
+
+                foreach (var di in dir.GetDirectories())
+                {
+                    SetFolder(path + "/" + di.Name);
+                }
             }
 
-            foreach (var di in dir.GetDirectories())
-            {
-                ClearFolder(di.FullName);
-                di.Delete();
-            }
+            SetFolder(path.Split("\\").Last());
         }
     }
 }
